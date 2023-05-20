3307bde93f12df6c935155be9fe5efea6595930e
duyonix <dtduy2210@gmail.com>
2023-05-20 11:43:25 +0700

    fix calculate getDistance

--- src/main/java/com/onix/worldtour/util/Util.java
+++ src/main/java/com/onix/worldtour/util/Util.java
@@ -7,8 +7,24 @@ import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 public class Util {
+    private static final double EARTH_RADIUS = 6371.0; // Earth's mean radius in kilometers
     public static double getDistance(Coordinate x, Coordinate y) {
-        return Math.sqrt(Math.pow(x.getLattitude() - y.getLattitude(), 2) + Math.pow(x.getLongitude() - y.getLongitude(), 2));
+        double lat1 = Math.toRadians(x.getLattitude());
+        double lon1 = Math.toRadians(x.getLongitude());
+        double lat2 = Math.toRadians(y.getLattitude());
+        double lon2 = Math.toRadians(y.getLongitude());
+
+        double deltaLat = lat2 - lat1;
+        double deltaLon = lon2 - lon1;
+
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+                + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double distance = EARTH_RADIUS * c;
+
+        return distance;
     }
 
     public static LocalDateTime convertTimestampToDateTime(Long timestamp) {
